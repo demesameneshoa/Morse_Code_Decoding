23edf7ded71d2b4b0081cffc0ca4e9799e31b20e
Anwar <anwarhussaini160@gmail.com>
2023-08-24 05:12:23 +0430

    Resovle linters errors

--- morsedecoder.rb
+++ morsedecoder.rb
@@ -1,23 +1,23 @@
-# Decod a single character
 def decode_char(character)
-  morse_dict = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-                 '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-                 '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
-                 '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
+  morse_dict = {
+    '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',   '-..'  => 'D',   '.'    => 'E',
+    '..-.' => 'F',   '--.'  => 'G',   '....' => 'H',   '..'   => 'I',   '.---' => 'J',
+    '-.-'  => 'K',   '.-..' => 'L',   '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',
+    '.--.' => 'P',   '--.-' => 'Q',   '.-.'  => 'R',   '...'  => 'S',   '-'    => 'T',
+    '..-'  => 'U',   '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',   '-.--' => 'Y',
+    '--..' => 'Z'
+  }
 
   morse_dict[character]
 end
 
-# Decode a word
 def decode_word(word)
-  word.split(' ').map { |char| decode_char(char) }.join
+  word.split.map { |char| decode_char(char) }.join
 end
 
-# Decode a sentence
 def decode_morse(morse_code)
-  morse_code.split('   ').map { |word| decode_word(word) }.join(' ')
+  morse_code.strip.split('   ').map { |word| decode_word(word) }.join(' ')
 end
-
 
 charachter = '._'
 puts decode_char(charachter)
@@ -27,5 +27,3 @@ puts decode_word(word)
 
 sentence = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 puts decode_morse(sentence)
-
-
